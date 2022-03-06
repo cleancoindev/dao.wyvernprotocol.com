77bf44d787995f5a5b166ccdf4ecc0f3e95ba37b
c00lgeye1 <c00lgeye1@protonmail.com>
2022-03-06 09:29:52 -0600

    update dependencies and infura

--- poi.config.js
+++ poi.config.js
@@ -1,17 +1,24 @@
 module.exports = (options, req) => ({
-  transformModules: ['ipfs-api', 'cids', 'multihashes', 'is-ipfs', 'ipld-dag-pb', 'multiaddr', 'multihashing-async', 'peer-id', 'libp2p-crypto', 'web3', 'asn1.js',
-    'multicodec', 'ipfs-block', 'multibase', 'ipfs-unixfs', 'libp2p-crypto-secp256k1', 'peer-info'],
-  entry: './src/index.js',
-  html: {
-    title: 'Wyvern DAO',
-    description: 'Decentralized autonomous organization responsible for governing the Wyvern Exchange',
-    template: 'src/index.ejs'
+  babel: {
+    jsx: 'vue',
+    transpileModules: ['ipfs-api', 'cids', 'multihashes', 'is-ipfs', 'ipld-dag-pb', 'multiaddr', 'multihashing-async', 'peer-id', 'libp2p-crypto', 'web3', 'asn1.js',
+      'multicodec', 'ipfs-block', 'multibase', 'ipfs-unixfs', 'libp2p-crypto-secp256k1', 'peer-info', '@ethersproject', 'web3-utils']
   },
-  webpack (config) {
-    config.module.noParse = []
-    config.module.noParse.push(/dtrace-provider$/)
-    config.module.noParse.push(/safe-json-stringify$/)
-    config.node = {fs: 'empty'}
-    return config
+  entry: './src/index.js',
+  output: {
+    html: {
+      title: 'Wyvern DAO',
+      description: 'Decentralized autonomous organization responsible for governing the Wyvern Exchange',
+      template: 'src/index.ejs'
+    }
+  },
+  configureWebpack: {
+    node: {
+      fs: 'empty'
+    },
+    externals: [/dtrace-provider$/, /safe-json-stringify$/, /fs$/],
+    module: {
+      noParse: [/dtrace-provider$/, /safe-json-stringify$/]
+    }
   }
 })
